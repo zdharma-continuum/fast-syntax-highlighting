38a9881a588ab383b95bcb0e79ac45d2b6be0836
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-12-28 21:20:48 +0100

    Revert "plugin.zsh: Ignore exit-code of Zle-widget call, go ahead and highlight"
    This reverts commit de8d9558fc34e67334a73716eec87bc5afbe2e6a.

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -171,10 +171,7 @@ _zsh_highlight_cursor_moved()
 # $1 is name of widget to call
 _zsh_highlight_call_widget()
 {
-    # Changed for compatibility with zsh-autosuggestions
-    # Was: builtin zle "$@" && _zsh_highlight
-    builtin zle "$@"
-    _zsh_highlight
+  builtin zle "$@" && _zsh_highlight
 }
 
 # Rebind all ZLE widgets to make them invoke _zsh_highlights.
