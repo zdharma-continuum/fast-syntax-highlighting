17a28baad4ea6e4453a92c89f7f39a341c9d0387
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2018-06-09 20:06:57 +0200

    New chroma `-docker.ch' that verifies image ID passed to `image rm'

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -272,9 +272,10 @@ ZSH_HIGHLIGHT_MAXLENGTH=10000
 # Load zsh/parameter module if available
 zmodload zsh/parameter 2>/dev/null
 
-autoload -Uz -- is-at-least fast-theme fast-read-ini-file -fast-run-git-command -fast-make-targets
+autoload -Uz -- is-at-least fast-theme fast-read-ini-file -fast-run-git-command -fast-make-targets \
+                -fast-run-command
 autoload -Uz -- chroma/-git.ch chroma/-example.ch chroma/-grep.ch chroma/-perl.ch chroma/-make.ch \
-                chroma/-awk.ch chroma/-vim.ch chroma/-source.ch chroma/-sh.ch
+                chroma/-awk.ch chroma/-vim.ch chroma/-source.ch chroma/-sh.ch chroma/-docker.ch
 source "${ZERO:h}/fast-highlight"
 
 local __fsyh_theme
