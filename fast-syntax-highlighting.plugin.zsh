c780ce377059eb1d3e6190b6bcab9f9aef3633d1
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2018-06-10 11:30:15 +0200

    New chroma `-ssh.ch', tracks if user passes port to hostspec

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -276,7 +276,7 @@ autoload -Uz -- is-at-least fast-theme fast-read-ini-file -fast-run-git-command 
                 -fast-run-command
 autoload -Uz -- chroma/-git.ch chroma/-example.ch chroma/-grep.ch chroma/-perl.ch chroma/-make.ch \
                 chroma/-awk.ch chroma/-vim.ch chroma/-source.ch chroma/-sh.ch chroma/-docker.ch \
-                chroma/-autoload.ch
+                chroma/-autoload.ch chroma/-ssh.ch
 source "${ZERO:h}/fast-highlight"
 
 local __fsyh_theme
