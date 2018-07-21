22dec035fca459acb6a6e4ba0b59fd15e7003130
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2018-07-21 14:30:06 +0200

    test: Update -git test code

--- test/parse.zsh
+++ test/parse.zsh
@@ -87,6 +87,7 @@ git diff --cached
 git commit --allow-empty
 git checkout themes/zdharma.ini
 git commit --amend
+git commit -m \"Example commit message\"
 git tag -a 'v1.18' -m 'Here-string is highlighted, descriptor-variables passed to exec are correctly highlighted'
 git tag -l -n9
 git checkout cb66b11
