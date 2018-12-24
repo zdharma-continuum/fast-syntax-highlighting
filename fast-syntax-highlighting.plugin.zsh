67dbb4e90ee3559c3c7b3da0691e9e22f0ce59f2
plexigras <plexigras@aol.com>
2018-12-24 19:51:00 +0100

    -{hub,lab}.ch: New GitHub `hub' and GitLab `lab' chromas (using -git.ch)
    lab and hub chromas (#97)
    
    * created separate lab and hub chromas
    
    * added git alias support to lab and hub chromas

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -309,7 +309,7 @@ zmodload zsh/system 2>/dev/null
 
 autoload -Uz -- is-at-least fast-theme fast-read-ini-file -fast-run-git-command -fast-make-targets \
                 -fast-run-command
-autoload -Uz -- chroma/-git.ch chroma/-example.ch chroma/-grep.ch chroma/-perl.ch chroma/-make.ch \
+autoload -Uz -- chroma/-git.ch chroma/-hub.ch chroma/-lab.ch chroma/-example.ch chroma/-grep.ch chroma/-perl.ch chroma/-make.ch \
                 chroma/-awk.ch chroma/-vim.ch chroma/-source.ch chroma/-sh.ch chroma/-docker.ch \
                 chroma/-autoload.ch chroma/-ssh.ch chroma/-scp.ch chroma/-which.ch chroma/-printf.ch \
                 chroma/-ruby.ch chroma/-whatis.ch chroma/-alias.ch chroma/-subcommand.ch \
