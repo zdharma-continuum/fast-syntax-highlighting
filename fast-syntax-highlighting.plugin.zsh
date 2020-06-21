4bdc45b5067c865d9aada6c1d087a7d42b2cb5ae
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-06-21 22:25:59 +0200

    plugin.zsh: Load zsh/nearcolor

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -377,3 +377,5 @@ if [[ $(uname -a) = (#i)*darwin* ]] {
     typeset -gA FAST_HIGHLIGHT
     FAST_HIGHLIGHT[chroma-man]=
 }
+
+[[ $COLORTERM == (24bit|truecolor) || ${terminfo[colors]} -eq 16777216 ]] || zmodload zsh/nearcolor &>/dev/null
