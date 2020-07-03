865566ce48cfd9bb5cdbaf5b1a74b0a675f4ccd4
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2020-07-04 01:56:49 +0200

    plugin.zsh: Solve the permissions problems #185

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -53,7 +53,7 @@ then
     fpath+=( "${0:h}" )
 fi
 
-if [[ $FAST_WORK_DIR = (/usr|/opt)/* && ! -w $FAST_WORK_DIR ]]; then
+if [[ ! -w $FAST_WORK_DIR ]]; then
     FAST_WORK_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/fsh"
     command mkdir -p "$FAST_WORK_DIR"
 fi
