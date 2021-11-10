55fedda2de7dad38ffe334e4519b0da9dac8d89a
Philipp Schmitt <philipp@schmitt.co>
2021-11-10 18:27:26 +0100

    Update url

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -33,7 +33,7 @@
 # regardless of functionargzero and posixargzero,
 # and with an option for a plugin manager to alter
 # the plugin directory (i.e. set ZERO parameter)
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
