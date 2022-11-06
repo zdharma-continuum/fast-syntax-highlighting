a6648cf3f07a738dd2835c0254e747afa7d98f61
Grigorii Horos <horosgrisa@gmail.com>
2022-11-06 13:46:57 +0200

    Rename internal functionss

--- fast-syntax-highlighting.plugin.zsh
+++ fast-syntax-highlighting.plugin.zsh
@@ -269,7 +269,7 @@ _zsh_highlight_bind_widgets()
               zle -N -- $cur_widget _zsh_highlight_widget_$prefix-$cur_widget;;
 
       # Completion widget: override and rebind old one with prefix "orig-".
-      completion:*) zle -C $prefix-$cur_widget ${${(s.:.)widgets[$cur_widget]}[2,3]} 
+      completion:*) zle -C $prefix-$cur_widget ${${(s.:.)widgets[$cur_widget]}[2,3]}
                     eval "_zsh_highlight_widget_${(q)prefix}-${(q)cur_widget}() { _zsh_highlight_call_widget ${(q)prefix}-${(q)cur_widget} -- \"\$@\" }"
                     zle -N -- $cur_widget _zsh_highlight_widget_$prefix-$cur_widget;;
 
@@ -278,7 +278,7 @@ _zsh_highlight_bind_widgets()
                zle -N -- $cur_widget _zsh_highlight_widget_$prefix-$cur_widget;;
 
       # Incomplete or nonexistent widget: Bind to z-sy-h directly.
-      *) 
+      *)
          if [[ $cur_widget == zle-* ]] && [[ -z $widgets[$cur_widget] ]]; then
            _zsh_highlight_widget_${cur_widget}() { :; _zsh_highlight }
            zle -N -- $cur_widget _zsh_highlight_widget_$cur_widget
@@ -326,15 +326,15 @@ zmodload zsh/system 2>/dev/null
 
 autoload -Uz -- is-at-least fast-theme .fast-read-ini-file .fast-run-git-command \
                 .fast-make-targets .fast-run-command .fast-zts-read-all
-autoload -Uz -- →chroma/-git.ch →chroma/-hub.ch →chroma/-lab.ch →chroma/-example.ch \
-                →chroma/-grep.ch →chroma/-perl.ch →chroma/-make.ch →chroma/-awk.ch \
-                →chroma/-vim.ch →chroma/-source.ch →chroma/-sh.ch →chroma/-docker.ch \
-                →chroma/-autoload.ch →chroma/-ssh.ch →chroma/-scp.ch →chroma/-which.ch \
-                →chroma/-printf.ch →chroma/-ruby.ch →chroma/-whatis.ch →chroma/-alias.ch \
-                →chroma/-subcommand.ch →chroma/-autorandr.ch →chroma/-nmcli.ch \
-                →chroma/-fast-theme.ch →chroma/-node.ch →chroma/-fpath_peq.ch \
-                →chroma/-precommand.ch →chroma/-subversion.ch →chroma/-ionice.ch \
-                →chroma/-nice.ch →chroma/main-chroma.ch →chroma/-ogit.ch →chroma/-zinit.ch
+autoload -Uz -- -chroma/-git.ch -chroma/-hub.ch -chroma/-lab.ch -chroma/-example.ch \
+                -chroma/-grep.ch -chroma/-perl.ch -chroma/-make.ch -chroma/-awk.ch \
+                -chroma/-vim.ch -chroma/-source.ch -chroma/-sh.ch -chroma/-docker.ch \
+                -chroma/-autoload.ch -chroma/-ssh.ch -chroma/-scp.ch -chroma/-which.ch \
+                -chroma/-printf.ch -chroma/-ruby.ch -chroma/-whatis.ch -chroma/-alias.ch \
+                -chroma/-subcommand.ch -chroma/-autorandr.ch -chroma/-nmcli.ch \
+                -chroma/-fast-theme.ch -chroma/-node.ch -chroma/-fpath_peq.ch \
+                -chroma/-precommand.ch -chroma/-subversion.ch -chroma/-ionice.ch \
+                -chroma/-nice.ch -chroma/main-chroma.ch -chroma/-ogit.ch -chroma/-zinit.ch
 
 source "${0:h}/fast-highlight"
 source "${0:h}/fast-string-highlight"
